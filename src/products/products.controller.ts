b9faea6d362a54733938a616fbee95e17384d6f1
Eduardo Loeza <84247208+eduardoLOEZ@users.noreply.github.com>
2024-06-03 21:19:33 -0600

    avances con prisma

---
+++ src/products/products.controller.ts
@@ -0,0 +1,36 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query, ParseIntPipe } from '@nestjs/common';
+import { ProductsService } from './products.service';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { PaginationDTO } from 'src/common';
+import { MessagePattern } from '@nestjs/microservices';
+
+@Controller('products')
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
+  @Post()
+  create(@Body() createProductDto: CreateProductDto) {
+    return this.productsService.create(createProductDto);
+  }
+
+  @Get()
+  findAll(@Query() paginationDTO: PaginationDTO) {
+    return this.productsService.findAll(paginationDTO);
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.productsService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(@Param('id', ParseIntPipe) id: number, @Body() updateProductDto: UpdateProductDto) {
+    return this.productsService.update(id, updateProductDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.productsService.remove(+id);
+  }
+}
---
+++ src/products/products.service.ts
@@ -0,0 +1,91 @@
+import { Injectable, OnModuleInit, Logger, NotFoundException } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { PrismaClient } from '@prisma/client';
+import { PaginationDTO } from 'src/common';
+
+@Injectable()
+export class ProductsService extends PrismaClient implements OnModuleInit {
+  private readonly logger = new Logger("ProductsService")
+  onModuleInit() {
+    this.$connect();
+    this.logger.log("BASE DE DATOS CONECTADA...")
+  }
+  create(createProductDto: CreateProductDto) {
+    return this.product.create({data: createProductDto})
+  }
+
+  async findAll(paginationDTO: PaginationDTO) {
+
+    const {page, limit} = paginationDTO
+
+    const totalItems = await this.product.count({where: {available:true}
+    })
+    //retornar al usuario/cliente, el total de registros 
+
+    const lastPage =  Math.ceil( totalItems / limit)
+
+   return {
+    data: await this.product.findMany({
+      skip: (page - 1) * limit,
+      take: limit,
+      where: {available:true}
+    }),
+    meta: {
+      totalPages: totalItems, // registros de la DB
+      page: page, //mostrar la pagina actual en la que se encuentra
+      lastPage: lastPage
+
+    }
+
+    /*
+
+     "meta": {
+        "totalPages": 49,
+        "page": 1,
+        "lastPage": 5
+    }
+
+
+    */
+   }
+  }
+
+  async findOne(id: number) {
+    const product = await this.product.findFirst({
+      where: {id, available:true}
+    });
+
+    if(!product){
+      throw new NotFoundException(` Product with id: #${id} not found`);
+    }
+
+    return product;
+  }
+
+  async update(id: number, updateProductDto: UpdateProductDto) {
+
+    await this.findOne(id) //reutilizamos el metodo para ver si un id existe
+    return this.product.update({
+      where: {id},
+      data: updateProductDto
+    })
+  }
+
+  async remove(id: number) {
+    await this.findOne(id) //reutilizamos el metodo para ver si un id existe
+
+    const product = await this.product.update({
+      where: {id},
+      data: {
+        available: false
+      }
+    })
+
+    return product
+    /*return this.product.delete({
+      where: {id}
+    })
+    */
+  }
+}
